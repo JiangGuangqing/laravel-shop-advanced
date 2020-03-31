d9eb21c9a4ef523eceb90beb7239ed3da5ca8783
姜广庆 <hx@aumc.cc>
2020-03-31 11:27:32 +0800

    初始化分期付款模型

--- database/migrations/2020_03_30_123319_create_installment_items_table.php
+++ database/migrations/2020_03_30_123319_create_installment_items_table.php
@@ -15,6 +15,17 @@ class CreateInstallmentItemsTable extends Migration
     {
         Schema::create('installment_items', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->unsignedBigInteger('installment_id');
+            $table->foreign('installment_id')->references('id')->on('installments')->onDelete('cascade');
+            $table->unsignedInteger('sequence');
+            $table->decimal('base');
+            $table->decimal('fee');
+            $table->decimal('fine')->nullable();
+            $table->dateTime('due_date');
+            $table->dateTime('paid_at')->nullable();
+            $table->string('payment_method')->nullable();
+            $table->string('payment_no')->nullable();
+            $table->string('refund_status')->default(\App\Models\InstallmentItem::REFUND_STATUS_PENDING);
             $table->timestamps();
         });
     }
