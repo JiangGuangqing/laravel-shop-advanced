9393ffc9af070cf3fb48e7cee72ad303eab9535c
姜广庆 <hx@aumc.cc>
2020-04-03 14:41:35 +0800

    计算逾期费

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,6 +27,7 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')
         //          ->hourly();
         $schedule->command('corn:finish-crowdfunding')->everyMinute();
+        $schedule->command('cron:calculate-installment-fine')->daily();
     }
 
     /**
